7395880012b7937da136eaf53f1795ffead21831
sejong <sejong.kang@dslab.global>
2022-02-05 19:19:01 +0900

    RestController 추가

--- src/main/java/com/k3j/jwtlogin/api/UserResource.java
+++ src/main/java/com/k3j/jwtlogin/api/UserResource.java
@@ -1,16 +1,19 @@
 package com.k3j.jwtlogin.api;
 
+import com.k3j.jwtlogin.domain.Role;
 import com.k3j.jwtlogin.domain.User;
 import com.k3j.jwtlogin.service.UserService;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
 public class UserResource {
@@ -21,4 +24,29 @@ public class UserResource {
     public ResponseEntity<List<User>>getUsers() {
         return ResponseEntity.ok().body(userService.getUsers());
     }
+
+    @PostMapping("/user/save")
+    public ResponseEntity<User>saveUser(@RequestBody User user){
+        URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/user/save").toUriString());
+        return ResponseEntity.created(uri).body(userService.saveUser(user));
+    }
+
+    @PostMapping("/role/save")
+    public ResponseEntity<Role>saveRole(@RequestBody Role role){
+        URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/role/save").toUriString());
+        return ResponseEntity.created(uri).body(userService.saveRole(role));
+    }
+
+    @PostMapping("/role/addtouser")
+    // <?> : 와일드 카드 형태. 어떤 클래스인지 모름.
+    public ResponseEntity<?>addRoleToUser(@RequestBody RoleToUserForm form){
+        userService.addRoleToUser(form.getEmail(), form.getRoleName());
+        return ResponseEntity.ok().build();
+    }
+}
+
+@Data
+class RoleToUserForm{
+    private String email;
+    private String roleName;
 }
