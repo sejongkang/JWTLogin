e40e6c2f29f4822be44330477542f1dbfcdf02d0
sejong <sejong.kang@dslab.global>
2022-02-05 21:10:04 +0900

    UserDetailsService 구현 추가

--- src/main/java/com/k3j/jwtlogin/service/UserServiceImpl.java
+++ src/main/java/com/k3j/jwtlogin/service/UserServiceImpl.java
@@ -6,21 +6,30 @@ import com.k3j.jwtlogin.repo.RoleRepo;
 import com.k3j.jwtlogin.repo.UserRepo;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 @Transactional
-public class UserServiceImpl implements UserService {
+// UserDetailsService를 구현해서 Spring Security에서 UserServiceImple을 사용하도록 함.
+// Service
+public class UserServiceImpl implements UserService, UserDetailsService {
 
     // Autowired 대신, 생성자 주입.
     private final UserRepo userRepo;
     private final RoleRepo roleRepo;
 
+    // 회원가입 엔드포인트 구현해야함.
     @Override
     public User saveUser(User user) {
         return userRepo.save(user);
@@ -48,5 +57,22 @@ public class UserServiceImpl implements UserService {
     @Override
     public List<User> getUsers() {
         return userRepo.findAll();
+    }
+
+    // UserDetailService의 기본 함수 구현.
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+//        Optional<User> user = userRepo.findByUsername(username);
+//        if(!user.isPresent()){
+//            throw new UsernameNotFoundException("user not found");
+//        }
+
+        Optional<User> user = Optional.ofNullable(userRepo.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("no tfound")));
+        // ArrayList는 가변적 크기.
+        Collection<SimpleGrantedAuthority> authorities = new ArrayList<>();
+        user.get().getRoles().forEach(role ->
+                authorities.add(new SimpleGrantedAuthority(role.getName())));
+        // Entity User 클래스 조회해서 Security User 클래스로 반환.
+        return new org.springframework.security.core.userdetails.User(user.get().getUsername(), user.get().getPassword(), authorities);
     }
 }
