a98f1fe21148215ef4076b91c2becf0c1405619f
sejong <sejong.kang@dslab.global>
2022-02-05 21:09:14 +0900

    커스텀 인증필터 작성

--- src/main/java/com/k3j/jwtlogin/filter/CustomAuthenticationFilter.java
+++ src/main/java/com/k3j/jwtlogin/filter/CustomAuthenticationFilter.java
@@ -1,2 +1,43 @@
-package com.k3j.jwtlogin.filter;public class CustomAuthenticationFilter {
+package com.k3j.jwtlogin.filter;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Slf4j
+@RequiredArgsConstructor
+public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+    private final AuthenticationManager authenticationManager;
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+        log.info("email is :{}", email);
+        // 인증용 객체 생성.
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(email, password);
+        // 이후 ProviderManager -> AuthenticationProvider -> UserDetailService -> UserDetails 순으로 확인.
+        return authenticationManager.authenticate(authenticationToken);
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
+        super.successfulAuthentication(request, response, chain, authResult);
+    }
+
+    @Override
+    protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, AuthenticationException failed) throws IOException, ServletException {
+        super.unsuccessfulAuthentication(request, response, failed);
+    }
 }
